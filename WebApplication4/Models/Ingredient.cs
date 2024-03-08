91a14d35148768d7cda928089490be942b9ed2c0
Dominik <frexorplay@gmail.com>
2024-03-08 19:48:20 +0100

    nothing important

--- WebApplication4/Models/Ingredient.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace WebApplication4.Models;
-
-public class Ingredient
-{
-    
-}
--- WebApplication4/Models/Recipe.cs
+++ WebApplication4/Models/Recipe.cs
@@ -1,6 +1,10 @@
-﻿namespace WebApplication4.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication4.Models;
 
 public class Recipe
 {
+    [Key]
+    private int RecipeId { get; set; }
     
 }
