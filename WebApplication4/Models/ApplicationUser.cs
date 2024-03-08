b1e754ecc9f3b57563df2a102b6bf494d71aec57
Maciej1302 <134784694+Maciej1302@users.noreply.github.com>
2024-03-08 21:19:51 +0100

    added models User and RecipeRanking

--- WebApplication4/Models/ApplicationUser.cs
+++ WebApplication4/Models/ApplicationUser.cs
@@ -1,6 +1,39 @@
-﻿namespace WebApplication4.Models;
+﻿using System.ComponentModel.DataAnnotations;
+namespace WebApplication4.Models;
 
 public class ApplicationUser
 {
+    [Key]
+    public int UserId { get; set; }
+    [Required(ErrorMessage = "Imię jest wymagane.")]
+    
+    [StringLength(50, ErrorMessage = "Imię nie może być dłuższe niż 50 znaków.")]
+    public string UserName { get; set; }
+    
+    [Required(ErrorMessage = "Nazwisko jest wymagane.")]
+    [StringLength(50, ErrorMessage = "Nazwisko nie może być dłuższe niż 50 znaków.")]
+    public string UserSurname { get; set; }
+    
+    [Required(ErrorMessage = "Adres email jest wymagany.")]
+    [EmailAddress(ErrorMessage = "Nieprawidłowy format adresu email.")]
+    public string UserEmail { get; set; }
+    
+    [Required(ErrorMessage = "Hasło jest wymagane.")]
+    [DataType(DataType.Password)]
+    public string UserPassword { get; set; }
+    
+    public virtual ICollection<ApplicationUser> Friends { get; set; }
+    public virtual ICollection<Recipe> Favorite_Recipes{ get; set; }
+
+    public ApplicationUser()
+    {
+        Friends = new HashSet<ApplicationUser>();
+    }
+    // DO Poprawy jak zainicjalizować te 2 listy ?
+    public Favorite_Recipes()
+    {
+        Favorite_Recipes = new HashSet<Recipe>();
+    }
+    
     
 }
---
+++ WebApplication4/Models/RecipeRanking.cs
@@ -0,0 +1,7 @@
+﻿namespace WebApplication4.Models;
+
+public class RecipeRanking
+{
+    public int RecipeID { get; set; }
+    public int RecipePlace { get; set; }
+}
