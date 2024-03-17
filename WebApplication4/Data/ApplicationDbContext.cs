3e897674d0d71a8c55b57e9a3be1e445d76c18f8
Dominik <frexorplay@gmail.com>
2024-03-17 23:12:00 +0100

    first migrations

--- WebApplication4/Data/ApplicationDbContext.cs
+++ WebApplication4/Data/ApplicationDbContext.cs
@@ -1,6 +1,16 @@
-﻿namespace WebApplication4.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication4.Models;
 
-public class ApplicationDbContext
+public class ApplicationDbContext : DbContext
 {
-    
+    public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+    public DbSet<AppRating> AppRatings { get; set; }
+    public DbSet<Ingredient> Ingredients { get; set; }
+    public DbSet<Recipe> Recipes { get; set; }
+    public DbSet<RecipeRanking> RecipeRankings { get; set; }
+
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
 }
