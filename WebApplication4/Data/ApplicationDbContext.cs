b1c5350716a8feb721c2474023417f7d0fc8371f
Maciej1302 <roziomaciej@gmail.com>
2024-04-07 14:42:56 +0200

    models finished

--- WebApplication4/Data/ApplicationDbContext.cs
+++ WebApplication4/Data/ApplicationDbContext.cs
@@ -3,14 +3,25 @@ using WebApplication4.Models;
 
 public class ApplicationDbContext : DbContext
 {
-    public DbSet<ApplicationUser> ApplicationUsers { get; set; }
-    public DbSet<AppRating> AppRatings { get; set; }
-    public DbSet<Ingredient> Ingredients { get; set; }
-    public DbSet<Recipe> Recipes { get; set; }
-    public DbSet<RecipeRanking> RecipeRankings { get; set; }
-
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options)
     {
+    }
+
+    public DbSet<User> Users { get; set; }
+    public DbSet<Recipe> Recipes { get; set; }
+    public DbSet<Ingredient> Ingredients { get; set; }
+    public DbSet<RecipeIngredient> RecipeIngredients { get; set; }
+    public DbSet<RecipeRanking> RecipeRankings { get; set; }
+    public DbSet<UserRanking> UserRankings { get; set; }
+    public DbSet<AppRating> AppRatings { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        // Definiowanie klucza złożonego dla RecipeIngredient
+        modelBuilder.Entity<RecipeIngredient>()
+            .HasKey(ri => new { ri.RecipeId, ri.IngredientId });
     }
 }
---
+++ WebApplication4/Models/User.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication4.Models;
+
+public class User
+{
+    [Key]
+    public Guid Id { get; set; }
+
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; }
+
+    [Required]
+    [StringLength(50, ErrorMessage = "Username cannot be longer than 50 characters.")]
+    public string Username { get; set; }
+
+    [Required]
+    [StringLength(50, ErrorMessage = "Surname cannot be longer than 50 characters.")]
+    public string UserSurname { get; set; }
+
+    [Required]
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
+
+    public virtual ICollection<Recipe> FavoriteRecipes { get; set; }
+    public virtual ICollection<User> Friends { get; set; }
+}
