1bd6aa65c9842588a9ec3ad29b0992b80dbd8e95
Dominik <frexorplay@gmail.com>
2024-05-22 18:12:24 +0200

    app rating feature added

--- WebApplication4/Controllers/AppRatingController.cs
+++ WebApplication4/Controllers/AppRatingController.cs
@@ -1,151 +1,53 @@
-using System;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using WebApplication4.Models;
+using WebApplication4.Services;
+using System.Threading.Tasks;
 
 namespace WebApplication4.Controllers
 {
     public class AppRatingController : Controller
     {
         private readonly ApplicationDbContext _context;
+        private readonly UserService _userService;
 
-        public AppRatingController(ApplicationDbContext context)
+        public AppRatingController(ApplicationDbContext context, UserService userService)
         {
             _context = context;
+            _userService = userService;
         }
 
-        // GET: AppRating
-        public async Task<IActionResult> Index()
+        // GET: AppRating/Rate
+        [HttpGet]
+        public IActionResult Rate()
         {
-            var applicationDbContext = _context.AppRating.Include(a => a.User);
-            return View(await applicationDbContext.ToListAsync());
-        }
-
-        // GET: AppRating/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var appRating = await _context.AppRating
-                .Include(a => a.User)
-                .FirstOrDefaultAsync(m => m.RatingId == id);
-            if (appRating == null)
-            {
-                return NotFound();
-            }
-
-            return View(appRating);
-        }
-
-        // GET: AppRating/Create
-        public IActionResult Create()
-        {
-            ViewData["UserId"] = new SelectList(_context.User, "Id", "Email");
             return View();
         }
 
-        // POST: AppRating/Create
+        // POST: AppRating/Rate
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create(AppRating appRating)
+        public async Task<IActionResult> Rate(int value)
         {
-            // Bez walidacji, po prostu dodaj do bazy danych
-            _context.Add(appRating);
+            var email = User.Identity.Name;
+            var user = await _userService.GetUserByEmailAsync(email);
+
+            if (user == null)
+            {
+                return NotFound("User not found.");
+            }
+
+            var rating = new AppRating
+            {
+                User = user,
+                Value = value
+            };
+
+            _context.AppRating.Add(rating);
             await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
 
-        // GET: AppRating/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var appRating = await _context.AppRating.FindAsync(id);
-            if (appRating == null)
-            {
-                return NotFound();
-            }
-
-            ViewData["UserId"] = new SelectList(_context.User, "Id", "Email", appRating.UserId);
-            return View(appRating);
-        }
-
-        // POST: AppRating/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("RatingId,UserId,Value")] AppRating appRating)
-        {
-            if (id != appRating.RatingId)
-            {
-                return NotFound();
-            }
-
-            try
-            {
-                _context.Update(appRating);
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!AppRatingExists(appRating.RatingId))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-            return RedirectToAction(nameof(Index));
-        }
-
-        // GET: AppRating/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var appRating = await _context.AppRating
-                .Include(a => a.User)
-                .FirstOrDefaultAsync(m => m.RatingId == id);
-            if (appRating == null)
-            {
-                return NotFound();
-            }
-
-            return View(appRating);
-        }
-
-        // POST: AppRating/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var appRating = await _context.AppRating.FindAsync(id);
-            if (appRating != null)
-            {
-                _context.AppRating.Remove(appRating);
-            }
-
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
-
-        // Sprawdza istnienie oceny w bazie danych
-        private bool AppRatingExists(int id)
-        {
-            return _context.AppRating.Any(e => e.RatingId == id);
+            TempData["SuccessMessage"] = "Thank you for rating the app!";
+            return RedirectToAction("Menu", "User");
         }
     }
 }
