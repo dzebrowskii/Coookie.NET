927d4378fc9c1ef5ba874e76db7be45361a361d4
Dominik <frexorplay@gmail.com>
2024-06-04 00:49:06 +0200

    Migrations

---
+++ WebApplication4/Migrations/20240603215618_AddCaloriesAndPriceToRecipe.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApplication4.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCaloriesAndPriceToRecipe : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Calories",
+                table: "Recipe",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Price",
+                table: "Recipe",
+                type: "decimal(18,2)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Calories",
+                table: "Recipe");
+
+            migrationBuilder.DropColumn(
+                name: "Price",
+                table: "Recipe");
+        }
+    }
+}
